ebc337ed936d72abdaff85fed8c85d521cc50df4
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-18 18:16:13 -0400

    replacing ref with Index

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -630,8 +630,8 @@ def type_ref(ir)->bool:
     print_token_type(ir.variable_left)
     #check if the right value already has a type?
     if not(is_type_undef(ir.variable_left)):
-        convert_ssa(ir.lvalue)
         copy_token_type(ir.lvalue, ir.variable_left)
+        print_token_type(ir.lvalue)
         print("x")
         return False
 
