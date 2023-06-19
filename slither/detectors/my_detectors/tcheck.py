08e914a08ad55ca1cd54d0ad23ada89eb5378795
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-19 16:19:56 -0400

    begining unit tests for references

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -1364,6 +1364,8 @@ def _tcheck_contract_state_var(contract):
         state_var.parent_function = "global"
         if(is_type_undef(state_var)):
             querry_type(state_var)
+            if(isinstance(state_var, ReferenceVariable)):
+                add_ref(state_var.name, (state_var.token_typen, state_var.token_typed, state_var.norm, state_var.link_function))
 
 #USAGE: labels which contracts that should be read (contains binary operations) also adds contract-function pairs
 #RTURNS: NULL
--- tests/detectors/backdoor/0.7.6/Unit_testing_suite/new_ref_handling/A.sol
+++ tests/detectors/backdoor/0.7.6/Unit_testing_suite/new_ref_handling/A.sol
@@ -1,18 +1,35 @@
 pragma solidity 0.8.3;
 
 contract A{
-    int a = 0;
+    uint a = 0;
     //make mapping here
-    function bool_array_map() returns (int){
+    mapping(address => bool) public address_bool_map;
+    bool[] bool_array;
+    mapping(address => int) public address_balance_map;
+    int[] balances;
+    mapping(address => mapping(int => int)) public d_2_map;
+    int[] decimals;
+
+    function bool_array_map(address member) public returns (uint){
+        bool d = address_bool_map[member];
+        bool e = bool_array[a];
         a = 1+1;
+        return a;
     }
-    function 1_dim_array_map() returns (int){
+    function d_1_array_map(address member) public returns (uint){
+        int d = address_balance_map[member];
+        int e = balances[a];
         a = 1+1;
+        return a;
     }
-    function 2_dim_array_map() returns (int){
+    function d_2_array_map(address member) public returns (uint){
+        int d = d_2_map[member][1];
         a = 1+1;
+        return a;
     }
-    function decimals_transformation() returns (int){
+    function decimals_transformation(uint normalized_token_amt) public returns (uint){
+	int d = decimals[normalized_token_amt];
         a = 1+1;
+        return a;
     }
 }
