230ed1e0af9eb1d69d718fc3b588a25b8f814b71
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-20 18:12:09 -0400

    changing binary functions to support ABSTRACT types| bug fix

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -890,7 +890,7 @@ def type_bin_add(dest, lir, rir) -> bool:
         add_errors(dest)
         return False
     else:
-        return copy_token_type(dest, tcheck.propagation.greater_abstract(rir, lir))
+        return copy_token_type(dest, tcheck_propagation.greater_abstract(rir, lir))
 
 #USAGE: typechecks subtraction statements
 #RETURNS: 'TRUE' if the node needs to be added back to the worklist
