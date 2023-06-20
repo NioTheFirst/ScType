e36c18bc5ea65a4fe574f69ae38c43248bfb688f
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-20 11:12:08 -0400

    add helper file for propagation to support ABSTRACT types| bug fix

--- slither/detectors/my_detectors/tcheck_propagation.py
+++ slither/detectors/my_detectors/tcheck_propagation.py
@@ -22,7 +22,7 @@ def _compare_token_type(A_types, B_types):
     A_buffer = 1
     B_buffer = 1
     B_pos = len(B_types)-1
-    for i in range(len(A_types), -1, -1):
+    for i in range(len(A_types)-1, -1, -1):
         if(A_types[i]>=abs_buf):
             if(A_types[i] > B_types[Bpos]):
                 B_buffer+=1
