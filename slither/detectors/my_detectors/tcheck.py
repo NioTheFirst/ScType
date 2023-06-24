552bcc8a4616c8a5df29301beae9b1fa4ec6b971
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-24 15:21:04 -0400

    debugging internal call return saving

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -490,7 +490,7 @@ def convert_ssa(ir):
     #    return
     non_ssa_ir = ir.non_ssa_version
     #name = ir.ssa_name
-    if(not (is_type_undef(non_ssa_ir)) and is_type_undef(ir)):
+    if(not (is_type_undef(non_ssa_ir))): # and is_type_undef(ir)):
         copy_token_type(non_ssa_ir, ir)
         print_token_type(ir)
         ir.norm = non_ssa_ir.norm
