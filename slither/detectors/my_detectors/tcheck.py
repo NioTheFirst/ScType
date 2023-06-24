0ad826bad1998fc0775ea9057503a3b6a6813f2a
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-23 21:24:32 -0400

    debugging state variable phi unpack

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -481,12 +481,12 @@ def convert_ssa(ir):
     #if(not(ir.ssa_name)):
     #    return
     non_ssa_ir = ir.non_ssa_version
-    print("Phi copy")
-    print_token_type(non_ssa_ir)
     #name = ir.ssa_name
     if(not (is_type_undef(non_ssa_ir)) and is_type_undef(ir)):
         copy_token_type(non_ssa_ir, ir)
         print_token_type(ir)
+        ir.norm = non_ssa_ir.norm
+        ir.link_function = non_ssa_ir.link_function
 #edit checkdafadjfahjfa
 
 #given and ir, type check
