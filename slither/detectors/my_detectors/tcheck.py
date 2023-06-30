0e96e1952657cc2b63fdfd43a4ded7ac088a7812
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-30 12:46:50 -0400

    added inline field prop | debug

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -401,6 +401,9 @@ def copy_token_tuple(ir, tt):
         #is an oobject, may have fields
         for field_name, field in ir.type.type.elems.items():
             #search for type tuple in type file
+            print(_ir.function_name)
+            print(_ir.name)
+            print(fieled_name)
             field_tt = get_field(_ir.function_name, _ir.name, field_name)
             if(field_tt):
                 _field_tt = field.extok
