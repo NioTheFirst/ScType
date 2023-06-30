0299b2ce4c98e837ab7925e2b7f09f285344cd94
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-30 17:21:50 -0400

    added inline field prop | debug

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -285,6 +285,9 @@ def read_type_file(ir):
     #print("read parent name: " + ir.name)
     if(_ir.name == None):
         return None
+    ref_tt = get_ref(var_name)
+    if(ref_tt):
+        return(ref_tt)
     return tcheck_parser.get_var_type_tuple(function_name, var_name)
 
 #USAGE: querry the user for a type
@@ -1627,11 +1630,11 @@ def _tcheck_contract_state_var(contract):
     for state_var in _read_state_variables(contract):
         print("State_var: "+state_var.name)
         state_var.parent_function = "global"
-        check_type(state_var)
-        """if(is_type_undef(state_var)):
+        #check_type(state_var)
+        if(is_type_undef(state_var)):
             querry_type(state_var)
             if(isinstance(state_var, ReferenceVariable)):
-                add_ref(state_var.name, (state_var.token_typen, state_var.token_typed, state_var.norm, state_var.link_function))"""
+                add_ref(state_var.name, (state_var.token_typen, state_var.token_typed, state_var.norm, state_var.link_function))
 #USAGE: labels which contracts that should be read (contains binary operations) also adds contract-function pairs
 #RTURNS: NULL
 def _mark_functions(contract):
