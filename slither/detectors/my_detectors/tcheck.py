482e8dc384a03e7e9e71afe92a3c89c0395566e9
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-29 22:29:57 -0400

    finish name propagation for fields, moving on to testing on rwc

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -756,6 +756,7 @@ def type_member(ir)->bool:
     print(_lv.name)
     print(_rv.name)
     print(pf_name)
+    print(ir.lvalue.type)
     if is_type_undef(ir.variable_left):
         print("UNDEFINED LEFT VARIABLE IN MEMBER")
         return True
