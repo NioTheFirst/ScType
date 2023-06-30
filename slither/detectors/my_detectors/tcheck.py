9fdca02fa866e1fbd5b835f2abb4a8bd81ffe2f6
NioTheFirst <bzhangprogramming@gmail.com>
2023-06-30 17:43:37 -0400

    added inline field prop | debug

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -414,6 +414,8 @@ def copy_token_tuple(ir, tt):
         _ir.norm = tt[2][0]
     _ir.linked_contract = tt[3]
 
+    if(isinstance(ir.type, UserDefinedType):
+            print(ir.type.type.elems)
     #Field tuple propagation
     if(isinstance(ir.type, UserDefinedType) and isinstance(ir.type.type, Structure)):
         #is an oobject, may have fields
