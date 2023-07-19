a6f9092e8acb9b0a6e0a4610a1ab2770f74c40ae
NioTheFirst <bzhangprogramming@gmail.com>
2023-07-19 11:12:30 -0400

    remove duplicate errors

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -1255,7 +1255,7 @@ def asn_norm(ir, norm):
        _ir.norm = '*'
     else:
         if(_ir.norm != norm or (_ir.norm == norm and norm == '*')):
-            if(_ir.norm == 0):
+            if(_ir.norm == 0 and norm != '*'):
                 _ir.norm = norm
                 return
             add_errors(ir)
