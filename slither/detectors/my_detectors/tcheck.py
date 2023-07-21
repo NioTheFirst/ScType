9d4b6897ec13b92351e02ce06e039a656fceb1a9
NioTheFirst <bzhangprogramming@gmail.com>
2023-07-21 17:16:05 -0400

    clearing incomplete

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -546,7 +546,7 @@ def check_type(ir) -> bool:
         addback = type_asn(ir.lvalue, ir.rvalue)
         print(get_norm(ir.rvalue))
         rnorm = get_norm(ir.rvalue)
-        print(is_constant(ir.rvalue))
+        print(f"is constnat? + {is_constant(ir.rvalue)}")
         if(ir.lvalue.extok.norm != '*' and not (is_constant(ir.rvalue) and rnorm == 0)):
             asn_norm(ir.lvalue, rnorm)
         pass_ftype(ir.lvalue, ir.rvalue, "assign")
