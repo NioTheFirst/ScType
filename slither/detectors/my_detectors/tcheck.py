f34f801029fe6a85b70f7405a6635775fb135b5c
NioTheFirst <bzhangprogramming@gmail.com>
2023-07-20 21:22:45 -0400

    add msg.sender to type conversion bin

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -541,7 +541,8 @@ def check_type(ir) -> bool:
         print("asgn")
         addback = type_asn(ir.lvalue, ir.rvalue)
         print(get_norm(ir.rvalue))
-        asn_norm(ir.lvalue, get_norm(ir.rvalue))
+        if(ir.lvalue.norm != '*'):
+            asn_norm(ir.lvalue, get_norm(ir.rvalue))
         pass_ftype(ir.lvalue, ir.rvalue, "assign")
         #print_token_type(ir.lvalue)
     elif isinstance(ir, Binary):
