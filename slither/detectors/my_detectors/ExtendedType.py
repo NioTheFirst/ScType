3567afe7716c178ea6805725bccc0b968b21db8b
NioTheFirst <bzhangprogramming@gmail.com>
2023-07-22 17:07:59 -0400

    add append typfile code

--- slither/detectors/my_detectors/ExtendedType.py
+++ slither/detectors/my_detectors/ExtendedType.py
@@ -120,8 +120,8 @@ class ExtendedType():
 
     @norm.setter
     def norm(self, a):
-        if(a == -404):
-            a = '*'
+        #if(a == -404):
+        #    a = '*'
         self._norm = a
     
     @property
