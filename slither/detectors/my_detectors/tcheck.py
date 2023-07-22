047314ce2eabf723f3b3915eb07a9ebcbc4f9df4
NioTheFirst <bzhangprogramming@gmail.com>
2023-07-22 10:11:10 -0400

    debugging pow

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -1772,7 +1772,7 @@ def _clear_type_node(node):
                 
 
                 print("[i] " + ir.lvalue.name + " cleared")
-                print(_ir)
+                #print(_ir)
         #if(debug_pow_pc):
         ###    for pc in debug_pow_pc:
         #      print("CCCCCC")
