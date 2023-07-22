2a39206780000e2ac737bdb5e128244dd753e31d
NioTheFirst <bzhangprogramming@gmail.com>
2023-07-21 22:56:27 -0400

    clearing incomplete

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -970,7 +970,8 @@ def type_fc(ir) -> bool:
     #if(not(mark_iteration) or current_function_marked):
     added = add_param_cache(ir.function, new_param_cache)
     for pc in ir.function.parameter_cache():
-        print_param_cache(pc)
+        for param in pc:
+            print_param_cache(param)
     if(added == -100):
         print("added")
         addback = _tcheck_function_call(ir.function, new_param_cache)
