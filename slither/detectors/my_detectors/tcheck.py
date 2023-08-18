b5675baa2774e989d1a57c315a663ba2cdb8ebfb
NioTheFirst <bzhangprogramming@gmail.com>
2023-08-18 16:39:45 -0400

    print out address markers

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -635,7 +635,7 @@ def check_type(ir) -> bool:
     elif isinstance(ir, TypeConversion):
         if(debug_print):
             print(ir)
-            print(ir.lvalue.extok)
+            print(ir.variable.extok)
         #convert_ssa(ir.lvalue)
         convert_ssa(ir.variable)
         if(str(ir.variable) == "this" or str(ir.variable) == "block.number" or str(ir.variable) == "msg.sender"):
