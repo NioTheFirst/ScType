73052af1c851781f8d3da57521095d7aa9ca4fe8
NioTheFirst <bzhangprogramming@gmail.com>
2023-08-18 18:26:38 -0400

    address copying temporary storage

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -701,8 +701,10 @@ def check_type(ir) -> bool:
 #RETURNS: nothing
 def type_conversion(ir):
     if(debug_print):
-            print(ir)
-            #print(ir.variable.extok)
+        print(ir)
+        #print(ir.variable.extok)
+        if(str(ir.variable.type) == "address"):
+            print("Is address")
         #convert_ssa(ir.lvalue)
     convert_ssa(ir.variable)
     if(str(ir.variable) == "this" or str(ir.variable) == "block.number" or str(ir.variable) == "msg.sender"):
