338b695fbe2455c78dcc0592539fac25eeaa4fcb
NioTheFirst <bzhangprogramming@gmail.com>
2023-08-21 12:29:17 -0400

    remove temp address reset

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2113,8 +2113,8 @@ def _tcheck_function_call(function, param_cache) -> []:
     global function_ref
     global function_count
     global temp_address_counter
-    save_temp = temp_address_counter
-    temp_address_counter = 0
+    #save_temp = temp_address_counter
+    #temp_address_counter = 0
     ##print("xyz")
     function_hlc = 0
     function_ref = 0
@@ -2184,7 +2184,7 @@ def _tcheck_function_call(function, param_cache) -> []:
         curlen = len(addback_nodes)
         ##print(f"WORKLIST iteration {wl_iter} for function call \"{function.name}\":\n New undefined nodes- {curlen}\n Old undefined nodes- {prevlen}")
         wl_iter+=1
-    temp_address_counter = save_temp
+    #temp_address_counter = save_temp
     return addback_nodes
 
 #USAGE: typecheck a function
@@ -2198,8 +2198,8 @@ def _tcheck_function(function) -> []:
     global mark_iteration
     global current_function_marked
     global temp_address_counter
-    save_temp = temp_address_counter
-    temp_address_counter = 0
+    #save_temp = temp_address_counter
+    #temp_address_counter = 0
     function_hlc = 0
     function_ref = 0
     explored = set()
@@ -2263,7 +2263,7 @@ def _tcheck_function(function) -> []:
         ##print(f"WORKLIST iteration {wl_iter} for function call \"{function.name}\":\n New undefined nodes- {curlen}\n Old undefined nodes- {prevlen}")
         wl_iter+=1
     #Save return value
-    temp_address_counter = save_temp
+    #temp_address_counter = save_temp
     handle_return(None, function)
     return addback_nodes
 
