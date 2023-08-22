a14ef93af48ababec1b3bd07d012325d9bcd5fab
NioTheFirst <bzhangprogramming@gmail.com>
2023-08-22 16:06:37 -0400

    create address_hanlder for address things

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -900,7 +900,7 @@ def querry_fc(ir) -> int:
             convert_ssa(ir.lvalue)
             copy_token_tuple(ir.lvalue, written_func_ret)
             #address
-            new_address(ir.lvaluem False)
+            new_address(ir.lvalue, False)
         elif(isinstance(ir.lvalue, TupleVariable) and len(written_func_rets) > 1):
             add_tuple(ir.lvalue.name, written_func_rets)
         else:
