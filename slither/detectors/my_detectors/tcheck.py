41e7fe17cf57e3bc9a91215aa4b8f22310b177f1
NioTheFirst <bzhangprogramming@gmail.com>
2023-08-24 21:40:06 -0400

    print local var

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2053,6 +2053,9 @@ def _tcheck_node(node, function) -> []:
     irs = []
     #local vars read
     print("Begin local variable read")
+    for lv in node.ssa_local_variables_read:
+        print(lv.extok)
+    print("ALLL")
     for local_var in node.ssa_variables_read:
         print(local_var.extok)
         #Load initial parameters mapping
