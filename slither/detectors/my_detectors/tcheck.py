45f1016f5ed772e06435c128c9bf35c6299c10c1
NioTheFirst <bzhangprogramming@gmail.com>
2023-09-17 11:56:15 -0400

    testing new typefile external function handling

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2312,11 +2312,11 @@ def _tcheck_node(node, function) -> []:
     for ir in node.irs_ssa:
         #DEFINE REFERENCE RELATIONS
         ir.dnode = node
-        if isinstance(ir, Phi):
+        #if isinstance(ir, Phi):
             #Phi de
-            lv = ir.lvalue
-            propogate_global(lv)
-            print(lv.extok)
+        #    lv = ir.lvalue
+        #    propogate_global(lv)
+        #    print(lv.extok)
         if isinstance(ir, Member):
             if isinstance(ir.lvalue, ReferenceVariable):
                 ir.lvalue.extok.ref([ir.variable_left, ir.variable_right])
