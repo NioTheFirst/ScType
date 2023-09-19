5058305618ac1ef29a709f91e3bb78ce3d80ec7c
NioTheFirst <bzhangprogramming@gmail.com>
2023-09-18 21:55:02 -0400

    financial meaning update to tcheck

--- slither/detectors/my_detectors/address_handler.py
+++ slither/detectors/my_detectors/address_handler.py
@@ -1,11 +1,28 @@
 #Handles address things
 #label to normalization
-import tcheck_parser
-from tcheck_parser import f_type_name, f_type_num, update_start, address_to_label, label_sets
+f_type_num = {
+    -1: "undef",
+    0: "raw balance",
+    1: "net balance",
+    2: "accrued balance",
+    3: "final balance",
+    10: "compound fee ratio (t)",
+    11: "transaction fee",  
+    12: "simple fee ratio",
+    13: "transaction fee (n)",
+    14: "transaction fee (d)",
+    20: "simple interest ratio",
+    21: "compound interest ratio",
+    22: "simple interest",
+    23: "compound interest",
+    30: "reserve",
+    40: "price/exchange rate",
+    50: "debt",
+}
 num_to_norm = {}
-#label_sets = {}
+label_sets = {}
 label_to_address = {}
-#address_to_label = {}
+address_to_label = {}
 global_address_counter = 0
 temp_address_counter = -1000
 
