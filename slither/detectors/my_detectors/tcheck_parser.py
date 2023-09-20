3f0c1e7b0c06adc255085f816d004fc4ff9acc13
NioTheFirst <bzhangprogramming@gmail.com>
2023-09-20 14:17:24 -0400

    testing on Benchmark 1 MarginSwap

--- slither/detectors/my_detectors/tcheck_parser.py
+++ slither/detectors/my_detectors/tcheck_parser.py
@@ -408,10 +408,12 @@ def get_tuple(tuple_name):
 
 def add_field(function_name, parent_name, field_name, type_tuples):
     key = function_name+'_'+parent_name+'_'+field_name
+    print(f"IN KEY: {key}")
     field_type_hash[key] = type_tuples
 
 def get_field(function_name, full_parent_name, field_name):
     key = function_name + '_' + full_parent_name + '_' + field_name
+    print(f"OUT KEY: {key}")
     if key in field_type_hash:
         return field_type_hash[key]
     return None
