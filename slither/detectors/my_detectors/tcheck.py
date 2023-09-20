f6fd2b349ea7d50a033f2263b3097bb2aa133b91
NioTheFirst <bzhangprogramming@gmail.com>
2023-09-20 14:37:05 -0400

    testing on Benchmark 1 MarginSwap

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -1128,7 +1128,7 @@ def type_member(ir)->bool:
     init_var(ir.variable_right)
     _lv = ir.variable_left.non_ssa_version.extok
     _lvname = ir.variable_left.ssa_name
-    _rv = ir.variable_right.non_ssa_version.extok
+    _rv = ir.variable_right.extok
     _ir = ir.lvalue.extok
     pf_name = _lv.function_name
     ##print(_lv.name)
