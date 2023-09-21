b9f0b433d4fec28292abe0387d15b821a185da58
NioTheFirst <bzhangprogramming@gmail.com>
2023-09-21 13:28:24 -0400

    refactor parser

--- slither/detectors/my_detectors/tcheck_propagation.py
+++ slither/detectors/my_detectors/tcheck_propagation.py
@@ -156,7 +156,8 @@ def copy_inv_token_type(src, dest):
 #RETURNS: nN/A
 def copy_pc_token_type(_src, dest):
     #src = copy.deepcopy(_src)
-    print(_src.extok)
+    #print(_src.extok)
+    src = _src
     _dest = dest.extok
     _dest.token_type_clear()
     for n in src[0]:
