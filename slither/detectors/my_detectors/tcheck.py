e2a02ad43dd460abc8124d9aee876e85e6025c79
NioTheFirst <bzhangprogramming@gmail.com>
2023-10-02 21:35:46 -0400

    test 8 resolving trtace

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2433,7 +2433,7 @@ def _tcheck_node(node, function) -> []:
             propogate_parameter(lv, function)
             propogate_global(lv)
             print(lv.extok)
-        
+        print("weee")
         if isinstance(ir, Member):
             if isinstance(ir.lvalue, ReferenceVariable):
                 ir.lvalue.extok.ref([ir.variable_left, ir.variable_right])
