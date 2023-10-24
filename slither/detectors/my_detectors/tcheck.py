f568c15bd518e0bf33ae7539f864f12b3d5cd72f
NioTheFirst <bzhangprogramming@gmail.com>
2023-10-23 22:19:11 -0400

    draft for special functions

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -964,13 +964,14 @@ def querry_fc(ir) -> int:
     dest = ir.destination
     #convert_ssa(dest)
     func_name = ir.function.name
-    '''
+    cont_name == None #TODO
+    
     if(isinstance(dest, Variable)):
         #DEPRECATED
         cont_name = dest.link_function
     else:
         cont_name = str(dest)
-    '''
+    
     #TODO
     ##print_token_type(dest)
     if(str(ir.lvalue.type) == "bool"):
