46ebed47eece5ee3f59ceca6268bcb3fe9b5063c
NioTheFirst <bzhangprogramming@gmail.com>
2023-10-23 22:08:33 -0400

    draft for special functions

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -1473,8 +1473,8 @@ def type_asn(dest, sorc) -> bool:
     #dest = ir.lvalue
     #sorc = ir.variable_right
     init_var(sorc)
-    #print("_______________")
-    #print(sorc.extok)
+    print("_______________")
+    print(sorc.extok)
     #print(dest.extok)
     #asn_norm(dest, get_norm(sorc))
     if(is_type_undef(sorc)):
