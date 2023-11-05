b5f5a4fdbd50d13e2f61e1ca2b6e820de70b52d0
NioTheFirst <bzhangprogramming@gmail.com>
2023-11-05 15:19:12 -0500

    Bug: Functions in certain contracts not showing up in project-wide detect

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2960,10 +2960,10 @@ def _mark_functions(contract):
         #print(function.nodes)
         fentry = {function.entry_point}
         #add contract-function pair
-        print(f"Mark functions Adding: {contract.name}, {function.name}")
+        print(f"Mark functions Adding: {contract.name}, {function.name}, {hasExternal}")
         #view_ir({function.entry_point})
         add_cf_pair(contract.name, function.name, function)
-        if(not (function.entry_point and (not hasExternal or function.visibility == "external" or function.visibility == "public"))):
+        if(not (function.entry_point and (not(hasExternal) or function.visibility == "external" or function.visibility == "public"))):
             function_check[function.name] = False
             ##print("[x] Not visible ")
             continue
