f9be4cc78b408abc4eccf85dd7ce45c52326cc13
NioTheFirst <bzhangprogramming@gmail.com>
2023-11-05 20:08:57 -0500

    Bug: Functions in certain contracts not showing up in project-wide detect

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -1013,7 +1013,7 @@ def querry_fc(ir) -> int:
         else:
             cont_name = str(dest.type)
     #Use original contract name instead of reduced name for interfaces etc.
-    included_func = get_cf_pair(str(dest.type), func_name)
+    included_func = get_cf_pair(cont_name, func_name)
     print(f"Found: {included_func}")
     if(included_func != None):
 
