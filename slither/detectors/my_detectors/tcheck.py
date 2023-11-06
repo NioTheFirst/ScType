c9264560cee5e4f7c4305ad2e989b083ba5447ca
NioTheFirst <bzhangprogramming@gmail.com>
2023-11-06 15:12:40 -0500

    Bug: finance type not propogating through fc

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2568,6 +2568,16 @@ def _tcheck_node(node, function) -> []:
     print("Propogating parameters and globals to SSA variables...")
     print(node.ssa_state_variables_read)
     print(node.ssa_local_variables_read)
+    for lv in node.local_state_variables_read:
+        print(lv)
+        #print(lv.extok)
+        propogate_parameter(lv, function)
+        propogate_global(lv)
+    for lv in node.ssa_state_variables_read:
+        print(lv)
+        #print(lv.extok)
+        propogate_parameter(lv, function)
+        propogate_global(lv)
     for lv in node.ssa_variables_read:
         #print(lv)
         #print(lv.extok)
