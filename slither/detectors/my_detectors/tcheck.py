0fcea6c003de856b8a11ca5b942389bfd3f81cf5
NioTheFirst <bzhangprogramming@gmail.com>
2023-12-22 10:04:14 -0500

    debugging multiplication

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2747,7 +2747,7 @@ def _clear_type_node(node):
                 _ir.token_type_clear()
                 _ir.norm = 'u'
                 _ir.finance_type = -1
-                for field in _lv.fields:
+                for field in _ir.fields:
                     field.extok.token_type_clear()
                 #update_non_ssa(ir.lvalue)
 
