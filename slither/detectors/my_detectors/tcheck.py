5b8c2e45d08d3b7c9c6cf380dd48e92282669039
NioTheFirst <bzhangprogramming@gmail.com>
2023-12-26 23:22:24 -0500

    add rule

--- slither/detectors/my_detectors/tcheck.py
+++ slither/detectors/my_detectors/tcheck.py
@@ -2280,7 +2280,8 @@ def _tcheck_node(node, function) -> []:
             temp.extok.function_name = function.name #"constructor"
             temp.extok.token_type_clear()
             copy_token_type(var, temp)
-            copy_ftype(var, temp)
+            if(temp.finance_type == -1):
+                copy_ftype(var, temp)
             temp.extok.norm = var.extok.norm
             global_var_types[(var.extok.name, current_contract_name)] = temp
             if(var.extok.address != 'u'):
